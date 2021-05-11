465c46453d94330e414a40692b15073152e197ad
Subhranil Mukherjee <subhranilmukherjee190@gmail.com>
2021-05-11 16:22:44 +0530

    fix: small fix in Pictures foldername

--- copy_and_push.sh
+++ copy_and_push.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 [ -d "~/storage/pictures"] && echo "This script must be run in termux after running 'termux-setup-storage'!\n" && exit
-cp -p "`ls -dtr1 ~/storage/Pictures/Screenshots/* | tail -1`" ss1.png
-cp -p "`ls -dtr1 ~/storage/Pictures/Screenshots/* | tail -2 | head 1`" ss2.png
+cp -p "`ls -dtr1 ~/storage/pictures/Screenshots/* | tail -1`" ss1.png
+cp -p "`ls -dtr1 ~/storage/pictures/Screenshots/* | tail -2 | head 1`" ss2.png
 git add ss1.png ss2.png
 git commit -m "wallpaper updated"
 USERNAME=`cat .username`
