acc93f429d99057d146869e191f273e7394b14f0
Subhranil Mukherjee <subhranilmukherjee190@gmail.com>
2021-05-11 16:37:10 +0530

    fix: copy_and_push: echo when -d returns 1

--- copy_and_push.sh
+++ copy_and_push.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-[ ! -d "~/storage/pictures" ] && echo "This script must be run in termux after running 'termux-setup-storage'!" && exit
+[ -d "~/storage/pictures" ] && echo "This script must be run in termux after running 'termux-setup-storage'!" && exit
+[ -d ".username" ] && echo "Enter your GitHub username (cached locally): " && read uname && echo $uname > .username
+[ -d ".password" ] && echo "Enter your GitHub password (cached locally): " && read passw && echo $passw > .password
 cp -p "`ls -dtr1 ~/storage/pictures/Screenshots/* | tail -1`" ss1.png
 cp -p "`ls -dtr1 ~/storage/pictures/Screenshots/* | tail -2 | head 1`" ss2.png
 git add ss1.png ss2.png
