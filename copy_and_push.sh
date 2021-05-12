24b2ccc3aff4e5e539a4a592452f829e7f2cea3b
Subhranil Mukherjee <subhranilmukherjee190@gmail.com>
2021-05-12 15:08:53 +0530

    updt: scripts: don't do anything if the ss not changed

--- copy_and_push.sh
+++ copy_and_push.sh
@@ -3,7 +3,13 @@
 [ -d "~/storage/pictures" ] && echo "This script must be run in termux after running 'termux-setup-storage'!" && exit
 [ -d ".username" ] && echo "Enter your GitHub username (cached locally): " && read uname && echo $uname > .username
 [ -d ".password" ] && echo "Enter your GitHub password (cached locally): " && read passw && echo $passw > .password
-cp -p "`ls -dtr1 ~/storage/pictures/Screenshots/* | tail -1`" ss1.png
+new_image=`ls -dtr1 ~/storage/pictures/Screenshots/* | tail -1`
+cmp "$new_image" ss1.png
+if [[ $? = 0 ]]
+then
+    echo "Image not changed! Exiting.."
+fi
+cp -p "$new_image" ss1.png
 git add ss1.png
 git commit -m "wallpaper updated"
 USERNAME=`cat .username`
--- copy_and_save.sh
+++ copy_and_save.sh
@@ -2,8 +2,16 @@
 
 [ -d "~/storage/pictures" ] && echo "This script must be run in termux after running 'termux-setup-storage'!" && exit
 [ -d "~/storage/shared" ] && echo "This script must be run in termux after running 'termux-setup-storage'!" && exit
+[ -d "~/storage/pictures/Screenshots" ] && echo "This script requires the screenshot directory to be present!" && exit
+new_image=`ls -dtr1 ~/storage/pictures/Screenshots/* | tail -1`
+cmp "$new_image" .last_image
+if [[ $? = 0 ]]
+then
+    echo "Image not changed! Exiting.."
+fi
 mv ss1.png local_ss1.png
-cp -p "`ls -dtr1 ~/storage/pictures/Screenshots/* | tail -1`" ss1.png
+cp -p "$new_image" ss1.png
+cp ss1.png .last_image
 python crop_wallpaper.py $1
 FOLDER=~/storage/shared/Kustom/BlurBacks
 mkdir -p $FOLDER
