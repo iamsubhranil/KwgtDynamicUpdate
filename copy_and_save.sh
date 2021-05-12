03e95e2bb4d9181a007e30386ccdea29b24f1090
Subhranil Mukherjee <subhranilmukherjee190@gmail.com>
2021-05-12 14:11:53 +0530

    fix: copy_and_save: few fixes

--- copy_and_save.sh
+++ copy_and_save.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-[ -d "~/storage/pictures" || -d "~/storage/shared" ] && echo "This script must be run in termux after running 'termux-setup-storage'!" && exit
+[ -d "~/storage/pictures" ] && echo "This script must be run in termux after running 'termux-setup-storage'!" && exit
+[ -d "~/storage/shared" ] && echo "This script must be run in termux after running 'termux-setup-storage'!" && exit
 cp -p "`ls -dtr1 ~/storage/pictures/Screenshots/* | tail -1`" local_ss1.png
-python crop_wallpaper.py $1
+python crop_wallpaper.py $1 && rm local_ss1.png && exit
 FOLDER=~/storage/shared/Kustom/BlurBacks
 mkdir -p $FOLDER
 mv date.png $FOLDER
