dd359b82ccbc548c3da404652e2661e0e68bb821
Subhranil Mukherjee <subhranilmukherjee190@gmail.com>
2021-05-12 15:21:01 +0530

    updt: copy_and_push: use github access token instead of password

--- copy_and_push.sh
+++ copy_and_push.sh
@@ -2,7 +2,7 @@
 
 [ -d "~/storage/pictures" ] && echo "This script must be run in termux after running 'termux-setup-storage'!" && exit
 [ -d ".username" ] && echo "Enter your GitHub username (cached locally): " && read uname && echo $uname > .username
-[ -d ".password" ] && echo "Enter your GitHub password (cached locally): " && read passw && echo $passw > .password
+[ -d ".token" ] && echo "Enter your GitHub token (cached locally): " && read tkn && echo $tkn > .token
 new_image=`ls -dtr1 ~/storage/pictures/Screenshots/* | tail -1`
 cmp "$new_image" ss1.png
 if [[ $? = 0 ]]
@@ -14,6 +14,6 @@ cp -p "$new_image" ss1.png
 git add ss1.png
 git commit -m "wallpaper updated"
 USERNAME=`cat .username`
-PASSWORD=`cat .password`
-git push https://$USERNAME:$PASSWORD@github.com/iamsubhranil/KwgtDynamicUpdate.git
+TOKEN=`cat .token`
+git push https://$USERNAME:$TOKEN@github.com/iamsubhranil/KwgtDynamicUpdate.git
 echo "Push to GitHub complete! Wait for a few seconds and then update the backgrounds.."
